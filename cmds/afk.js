051674ba8495f02bb6d8a538a356e85874a70b50
Glitch (ozhark-project) <none>
2020-06-19 06:28:30 +0000

    ⛷🚍 Checkpoint ./cmds/afk.js:16160679/85

--- cmds/afk.js
+++ cmds/afk.js
@@ -5,9 +5,9 @@ module.exports.run = (client, message, args) => {
     let reason = args.join(' ') || 'I am currently afk, I will reply as soon possible.';
     let afklist = client.afk.get(message.author.id);
 
-  if(args.join(' ')) {
+  if(reason) {
   client.user.setAFK(false);
-  message.reply(' i set you AFK')
+  message.reply(' i set you AFK:', reason)
   }
   
     
