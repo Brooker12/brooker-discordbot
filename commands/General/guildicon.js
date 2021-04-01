ee938837a1a2b0f550a712392b8b868b732fbc05
Glitch (ozhark-project) <none>
2021-04-01 04:20:57 +0000

    🍅🎙 Checkpoint ./commands/General/guildicon.js:16160679/249

--- commands/General/guildicon.js
+++ commands/General/guildicon.js
@@ -12,7 +12,8 @@ module.exports = {
 
   let embed = new Discord.MessageEmbed()
 
-    .setImage(message.guild.iconURL({dynamic: true, format: 'jpg', size: 2048}))
+    .setImage(message.guild.iconURL() ? message.guild.iconURL({dynamic: true, format: 'jpg', size: 2048}) 
+              : 'https://media.discordapp.net/attachments/804199427002073128/827033053003251752/unknown.png?width=543&height=480')
     .setColor(client.config.color)
     .setFooter("Searched by " + message.author.tag)
     .setDescription(`[${message.guild.name}](${message.guild.iconURL({dynamic: true, format: 'jpg', size: 2048})})`);
