ed948027da8cc93762b0576ce64cdab7396ba07a
Glitch (ozhark-project) <none>
2021-04-02 10:32:40 +0000

    🎋⏳ Checkpoint ./commands/Developer/partner.js:16160679/1591

--- commands/Developer/partner.js
+++ commands/Developer/partner.js
@@ -13,18 +13,22 @@ module.exports = {
     
   let partner = db.get(`partner`)
   
-  let guild = message.guild
+  let guild = client.guilds.cache.get(args[0])
+  if(!guild) return message.channel.send('Im not join dhat server')
   
+  let already = partner.find(x => x.id === guild.id)
+  if(already) return message.channel.send('This guild has been add to partner')
+    
   let invitechannels = guild.channels.cache.filter(c=> c.permissionsFor(guild.me).has('CREATE_INSTANT_INVITE'))
   if(!invitechannels) return message.channel.send('No Channels found with permissions to create Invite in!')
     
   invitechannels.random().createInvite().then(invite => {
     let data = {
-      id: message.guild.id,
+      id: guild.id,
       link: 'https://discord.gg/'+invite.code
     }
      db.push(`partner`, data)
     message.channel.send('Succesfully adding this server to Partner')
-   })  
-  }  
+   })    
+  }
 }
