da50c596d4905263292df0f9e09f53bc4532a4d6
Glitch (brooker) <none>
2021-04-05 01:00:00 +0000

    🐈🏹 Checkpoint ./views/manage-show.ejs:16160679/322 ./bot.js:16160679/116 ./config.json:16160679/493 ./util.js:16160679/1361 ./public/style.css:16160679/30932 ./lib/strategy.js:16160679/9774 ./handlers/command.js:16160679/2525 ./handlers/events.js:16160679/1762 ./handlers/missing.js:16160679/2441

--- handlers/missing.js
+++ handlers/missing.js
@@ -1,41 +1,41 @@
-const discord = require('discord.js')
-const db = require('quick.db')
+const discord = require("discord.js");
+const db = require("quick.db");
 
 class Util {
-  
   static getArgs(message, client, db) {
-    let prefix = db.fetch(`prefix_${message.guild.id}`)
-    if(prefix === null) prefix = "a."  
-    let cmd = message.content.split(prefix)[1].split(' ')[0]
-    
-    let command = client.commands.get(`${cmd}`) || client.commands.get(client.aliases.get(`${cmd}`))
-    
+    let prefix = db.fetch(`prefix_${message.guild.id}`);
+    if (prefix === null) prefix = "a.";
+    let cmd = message.content.split(prefix)[1].split(" ")[0];
+
+    let command =
+      client.commands.get(`${cmd}`) ||
+      client.commands.get(client.aliases.get(`${cmd}`));
+
     let Args = new discord.MessageEmbed()
-    .setColor('#2f3136')
-    .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-    .setDescription(`Usage: ${command.usage || "[ None ]"}`)
-    
-    return message.channel.send(Args)
+      .setColor("#2f3136")
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription(`Usage: ${command.usage || "[ None ]"}`);
+
+    return message.channel.send(Args);
   }
-  
+
   static getMention(message) {
     let Mention = new discord.MessageEmbed()
-    .setColor('#2f3136')
-    .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-    .setDescription(`You must mentions member first`)
-    
-   return message.channel.send(Mention)
+      .setColor("#2f3136")
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription(`You must mentions member first`);
+
+    return message.channel.send(Mention);
   }
-  
+
   static getInvalid(message) {
     let Invalid = new discord.MessageEmbed()
-    .setColor('#2f3136')
-    .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-    .setDescription(`Invalid Argument`)
-    
-    return message.channel.send(Invalid)
+      .setColor("#2f3136")
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription(`Invalid Argument`);
+
+    return message.channel.send(Invalid);
   }
-  
 }
 
 module.exports = Util;
--- util.js
+++ util.js
@@ -1,26 +1,28 @@
-const yes = ['yes'];
-const no = ['no']
+const yes = ["yes"];
+const no = ["no"];
 
 class Util {
-	static randomRange(min, max) {
-		return Math.floor(Math.random() * (max - min + 1)) + min;
-	}
-  
-	static async verify(channel, user, time = 30000) {
-		const filter = res => {
-			const value = res.content.toLowerCase();
-			return res.author.id === user.id && (yes.includes(value) || no.includes(value));
-		};
-		const verify = await channel.awaitMessages(filter, {
-			max: 1,
-			time
-		});
-		if (!verify.size) return 0;
-		const choice = verify.first().content.toLowerCase();
-		if (yes.includes(choice)) return true;
-		if (no.includes(choice)) return false;
-		return false;
-	}
+  static randomRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  static async verify(channel, user, time = 30000) {
+    const filter = res => {
+      const value = res.content.toLowerCase();
+      return (
+        res.author.id === user.id && (yes.includes(value) || no.includes(value))
+      );
+    };
+    const verify = await channel.awaitMessages(filter, {
+      max: 1,
+      time
+    });
+    if (!verify.size) return 0;
+    const choice = verify.first().content.toLowerCase();
+    if (yes.includes(choice)) return true;
+    if (no.includes(choice)) return false;
+    return false;
+  }
 }
 
-module.exports = Util; 
+module.exports = Util;
