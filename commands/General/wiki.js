923b09eefab30692bf132e0674ae3626da5ac936
Glitch (brooker) <none>
2021-04-06 16:22:50 +0000

    🚄💕 Checkpoint ./commands/General/wiki.js:16160679/1443

--- commands/General/wiki.js
+++ commands/General/wiki.js
@@ -1,4 +1,4 @@
-const Discord = require('discord.js');
+const {MessageEmbed} = require('discord.js');
 const fetch = require('node-fetch')
 
 module.exports = {
@@ -10,26 +10,24 @@ module.exports = {
   cooldown: 2000,
   run: async (client, message, args) => { 
     
-        if(!args[0])  return message.channel.send({embed: {
-                      color: client.config.color,
-                      title: "Input the query to search"
-                  }}) 
     
-        const body = await fetch(
-            `https://id.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(args.join(" "))}`,
-          ).then(res => res.json().catch(() => {}));
+        let xdemb = new MessageEmbed().setColor(client.config.color) 
+         .setAuthor("Missing Arguments!", message.author.displayAvatarURL())
+         .setDescription(`Usage: ${module.exports.usage}`)
+        if(!args[0]) return message.channel.send(xdemb) 
+    
+        const body = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(args.join(" "))}`)
+                           .then(res => res.json().catch(() => {}));
         
-        if (!body) return message.channel.send({embed: {
-                      color: client.config.color,
-                      title: "❌ Error Page Not Found."
-                  }})
-          if (body.title && body.title === "Not found.") return message.channel.send({embed: {
-                      color: client.config.color,
-                      title: "❌ Error Page Not Found."
-                  }});
+    
+        let error = new MessageEmbed().setColor(client.config.color) 
+         .setAuthor("Error Detect!", message.author.displayAvatarURL())
+         .setDescription(`Error Page Not Found.`)
+        if (!body) return message.channel.send(error)
+        if (body.title && body.title === "Not found.") return message.channel.send(error)
       
-        const embed = new Discord.MessageEmbed() .setColor(client.config.color)
-            .setAuthor(`${body.title}`, 'https://media.discordapp.net/attachments/801988747205935144/829023896140382318/Z.png')
+        const embed = new MessageEmbed() .setColor(client.config.color)
+            .setAuthor(`${body.title}`, 'https://images-ext-2.discordapp.net/external/ePG3u6WIV8mO0XbO5F_iml1O-XlHMXRlLG_TRdvSmvw/https/upload.wikimedia.org/wikipedia/en/thumb/8/80/Wikipedia-logo-v2.svg/320px-Wikipedia-logo-v2.svg.png')
             .addField("More Info: ",`**[Click Here!](${body.content_urls.desktop.page})**`, true)
             .setDescription(`** ${body.extract}**`)
         
