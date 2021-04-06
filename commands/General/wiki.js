b4fa2edd0301ffb35de8be20ee037e3b13934e83
Glitch (brooker) <none>
2021-04-06 16:10:48 +0000

    📊🌦 Checkpoint ./commands/General/wiki.js:16160679/927

--- commands/General/wiki.js
+++ commands/General/wiki.js
@@ -10,26 +10,28 @@ module.exports = {
   cooldown: 2000,
   run: async (client, message, args) => { 
     
+        if(!args[0])  return message.channel.send({embed: {
+                      color: client.config.color,
+                      title: "Input the query to search"
+                  }}) 
+    
         const body = await fetch(
-            `https://en.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(args.join(" "))}`,
+            `https://id.wikipedia.org/api/rest_v1/page/summary/${encodeURIComponent(args.join(" "))}`,
           ).then(res => res.json().catch(() => {}));
         
-        if (!body) return message.channel.sendmessage.channel.send({embed: {
-                      color: config.embedcolor,
+        if (!body) return message.channel.send({embed: {
+                      color: client.config.color,
                       title: "❌ Error Page Not Found."
                   }})
           if (body.title && body.title === "Not found.") return message.channel.send({embed: {
-                      color: config.embedcolor,
+                      color: client.config.color,
                       title: "❌ Error Page Not Found."
                   }});
       
-        const embed = new Discord.MessageEmbed()
-            .setTitle(`🌐 ${body.title} `)
-        .addField("More Info: ",`**[Click Here!](${body.content_urls.desktop.page})**`, true)
+        const embed = new Discord.MessageEmbed() .setColor(client.config.color)
+            .setAuthor(`${body.title}`, 'https://media.discordapp.net/attachments/801988747205935144/829023896140382318/Z.png')
+            .addField("More Info: ",`**[Click Here!](${body.content_urls.desktop.page})**`, true)
             .setDescription(`** ${body.extract}**`)
-            .setColor(config.embedcolor)
-        .setTimestamp()
-          .setFooter(`© Karma `, "https://cdn.discordapp.com/attachments/725019921159028808/739770316754256012/Screenshot_20200803-1459592.png")
         
          if (body.thumbnail) embed.setThumbnail(body.thumbnail.source);
         message.channel.send(embed);
