f5a3e4e92b11c6108312a789b8aa70de8a6c1d67
Glitch (brooker) <none>
2021-04-07 09:45:15 +0000

    🐵🚁 Checkpoint ./commands/Moderation/addrole.js:16160679/13072 ./commands/Moderation/removerole.js:16160679/24713 ./commands/Moderation/setnick.js:16160679/520

--- commands/Moderation/addrole.js
+++ commands/Moderation/addrole.js
@@ -1,4 +1,4 @@
-const {MessageEmbed} = require("discord.js");
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "addrole",
@@ -8,43 +8,74 @@ module.exports = {
   botPermission: ["MANAGE_ROLES"],
   authorPermission: ["MANAGE_ROLES"],
   aliases: [],
-  run: async (client, message, args) => {  
-    
-    
-    let ctx = new MessageEmbed().setColor(client.config.color)
-     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-     .setDescription("Mentions users first!")
-    if(!args[0]) return message.channel.send(ctx)
-    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === args[0].toLowerCase()) || message.guild.members.cache.find(x => x.displayName.toLowerCase() === args[0].toLowerCase())
-    
-    if(!user) return message.channel.send(ctx)
-    let ctxn = new MessageEmbed().setColor(client.config.color)
-     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-     .setDescription("I can't add role to this users")
-    if(!user.manageable) return message.channel.send(ctxn)
+  run: async (client, message, args) => {
+    let ctx = new MessageEmbed()
+      .setColor(client.config.color)
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription("**Mentions users first!**");
+    if (!args[0]) return message.channel.send(ctx);
 
-    let ctxa = new MessageEmbed().setColor(client.config.color)
-     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-     .setDescription("Mentions the roles to be added")
-    if(!args[1]) return message.channel.send(ctxa)
-    let roles = message.mentions.roles.first() || message.guild.roles.cache.get(args[1]) || message.guild.roles.cache.find(a => a.name.toLowerCase() === args[1].toLowerCase())
-    
-    if(!roles) return message.channel.send(ctxa)
-    let ctxb = new MessageEmbed().setColor(client.config.color)
-     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-     .setDescription("This users already have that roles")
-    if(user.roles.cache.has(roles.id)) return message.channel.send(ctxb)
-    let ctxz = new MessageEmbed().setColor(client.config.color)
-     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-     .setDescription("I can't acces that roles")
-    if(!roles.editable) return message.channel.send(ctxz)
-    
+    let user =
+      message.mentions.members.first() ||
+      message.guild.members.cache.get(args[0]) ||
+      message.guild.members.cache.find(
+        x => x.user.username.toLowerCase() === args[0].toLowerCase()
+      ) ||
+      message.guild.members.cache.find(
+        x => x.displayName.toLowerCase() === args[0].toLowerCase()
+      );
+
+    let ctxx = new MessageEmbed()
+      .setColor(client.config.color)
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription("**I can't get that user**");
+    let ctxn = new MessageEmbed()
+      .setColor(client.config.color)
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription("**I can't add role to this users**");
+
+    if (!user) return message.channel.send(ctxx);
+    if (!user.manageable) return message.channel.send(ctxn);
+
+    let ctxa = new MessageEmbed()
+      .setColor(client.config.color)
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription("**Mentions the roles to be addd**");
+    if (!args[1]) return message.channel.send(ctxa);
+
+    let roles =
+      message.mentions.roles.first() ||
+      message.guild.roles.cache.get(args[1]) ||
+      message.guild.roles.cache.find(
+        a => a.name.toLowerCase() === args[1].toLowerCase()
+      );
+
+    let ctxas = new MessageEmbed()
+      .setColor(client.config.color)
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription("**I can't get that role**");
+    let ctxb = new MessageEmbed()
+      .setColor(client.config.color)
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription("**This users already have that roles**");
+    let ctxz = new MessageEmbed()
+      .setColor(client.config.color)
+      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+      .setDescription("**I can't acces that roles**");
+
+    if (!roles) return message.channel.send(ctxas);
+    if (user.roles.cache.has(roles.id)) return message.channel.send(ctxb);
+    if (!roles.editable) return message.channel.send(ctxz);
+
     user.roles.add(roles).then(() => {
-      let embed = new MessageEmbed().setColor(client.config.color)
-       .setTitle('Mod: Add Roles')
-       .setDescription(`Succseffully remove **${roles.name}** to **${user.user.username}**`)
-       .setFooter(`Moderator: ${message.author.username}`)
-      message.channel.send(embed)
-    })    
+      let embed = new MessageEmbed()
+        .setColor(client.config.color)
+        .setTitle("Mod: Add Roles")
+        .setDescription(
+          `Succseffully add **${roles.name}** to **${user.user.username}**`
+        )
+        .setFooter(`Moderator: ${message.author.username}`);
+      message.channel.send(embed);
+    });
   }
-}
+};
--- commands/Moderation/setnick.js
+++ commands/Moderation/setnick.js
@@ -15,6 +15,11 @@ module.exports = {
      .setDescription("Mentions users first!")
     let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
     if(!user) return message.channel.send(ctx)
+
+    let ctxn = new MessageEmbed().setColor(client.config.color)
+     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+     .setDescription("You can't change nickname this users")
+    if(!user.manageable) return message.channel.send(ctxn)
     
     let ctxm = new MessageEmbed().setColor(client.config.color)
      .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
@@ -25,11 +30,6 @@ module.exports = {
     let newnick = args.slice(1).join(' ')
     if(newnick.length > 32) return message.channel.send(ctxw)
     if(!newnick) return message.channel.send(ctxm)
-    
-    let ctxn = new MessageEmbed().setColor(client.config.color)
-     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
-     .setDescription("You can't change nickname this users")
-    if(!user.manageable) return message.channel.send(ctxn)
     
     user.setNickname(newnick).then(() => {
       let embed = new MessageEmbed().setColor(client.config.color)
