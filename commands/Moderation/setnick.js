c76ef3b51b5a3eec32b91c6ba87afb24a5f2b0d7
Glitch (brooker) <none>
2021-04-07 05:45:09 +0000

    🏦💧 Checkpoint ./commands/Moderation/setnick.js:16160679/1753

--- commands/Moderation/setnick.js
+++ commands/Moderation/setnick.js
@@ -9,10 +9,28 @@ module.exports = {
   authorPermission: ["MANAGE_MESSAGES"],
   aliases: ["setnickname"],
   run: async (client, message, args) => { 
-  
-    let user = message.mentions.members.first() 
-    if(!user)
+
     
+    let ctx = new MessageEmbed().setColor(client.config.color)
+     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+     .setDescription("Mentions users first!")
+    let user = message.mentions.members.first() || message.guild.members.cache.get(args[0])
+    if(!user) return message.channel.send(ctx)
     
+    let ctxm = new MessageEmbed().setColor(client.config.color)
+     .setAuthor(`Missing Arguments!`, message.author.displayAvatarURL())
+     .setDescription("Enter new nickname for users")
+    let newnick = args.slice(1).join(' ')
+    if(!newnick) return message.channel.send(ctxm)
+    
+    if(user.editable)
+    
+    user.setNickname(newnick).then(() => {
+      let embed = new MessageEmbed().setColor(client.config.color)
+       .setTitle("Mod: Change Nickname")
+       .setDescription(`Succesfully change ${user.user.username} with ${newnick}`)
+       .setFooter(`Moderator: ${message.author.username}`)
+      message.channel.send(embed)
+    })
   }
 }
