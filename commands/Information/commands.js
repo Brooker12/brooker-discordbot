22b562650c509fd7baf9aad7639d5ff8cfbecefe
Glitch (brooker) <none>
2021-04-07 08:02:16 +0000

    🌋👫 Checkpoint ./commands/Information/commands.js:16160679/5

--- commands/Information/commands.js
+++ commands/Information/commands.js
@@ -58,7 +58,7 @@ let emx = new MessageEmbed().setColor(client.config.color)
         emx.addField("Custom Commands", array.join(", "))
       }
        emx.addField(`Links`,
-    `[Invite](https://brooker.cf/invite), [Vote](https://brooker.cf/vote), [Dashboard](https://brooker.cf/home)`)
+    `[Invite](https://brooker.cf/invite), [Vote](https://brooker.cf/vote), [Dashboard](https://brooker.cf)`)
       return message.channel.send(emx);
     }
   }
