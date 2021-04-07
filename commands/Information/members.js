6f70705a3f2dfb3c0b65a817954a8ac094e64d83
Glitch (brooker) <none>
2021-04-07 03:51:15 +0000

    👣💨 Checkpoint ./commands/Information/members.js:16160679/674

--- commands/Information/members.js
+++ commands/Information/members.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports = {
   name: "members",
-  aliases: ["mbs"],
+  aliases: ["mbs", "member"],
   category: "Information",
   description: "Show Discord Server Members!",
   usage: "Members",
@@ -12,24 +12,22 @@ module.exports = {
     const Bots = await message.guild.members.cache.filter(Mem => Mem.user.bot), 
           Humans = await message.guild.members.cache.filter(Mem => !Mem.user.bot), 
           Players = await message.guild.members.cache.filter(Mem => Mem.presence.activities && Mem.presence.activities[0] && Mem.presence.activities[0].type === "PLAYING"), 
-          Websites = await message.guild.members.cache.filter(Mem => !Mem.user.bot && Mem.presence.clientStatus && Object.keys(Mem.presence.clientStatus).includes("web")), 
-          Desktop = await message.guild.members.cache.filter(Mem => !Mem.user.bot && Mem.presence.clientStatus && Object.keys(Mem.presence.clientStatus).includes("desktop")), 
-          Mobile = await message.guild.members.cache.filter(Mem => !Mem.user.bot && Mem.presence.clientStatus && Object.keys(Mem.presence.clientStatus).includes("mobile")),
+          Websites = await message.guild.members.cache.filter(Mem => Mem.presence.clientStatus && Object.keys(Mem.presence.clientStatus).includes("web")), 
+          Desktop = await message.guild.members.cache.filter(Mem => Mem.presence.clientStatus && Object.keys(Mem.presence.clientStatus).includes("desktop")), 
+          Mobile = await message.guild.members.cache.filter(Mem => Mem.presence.clientStatus && Object.keys(Mem.presence.clientStatus).includes("mobile")),
           PercenH = await Math.round((message.guild.members.cache.filter(m => !m.user.bot).size / message.guild.memberCount) * 100),
           PercenB = await Math.round((message.guild.members.cache.filter(m => m.user.bot).size / message.guild.memberCount) * 100);
           
 
     const Embed = new Discord.MessageEmbed().setColor(client.config.color)
-    .setTitle("Members Information!")
-    .setDescription(`Total - **${message.guild.memberCount}**
-Human - **${Humans.size}** (${PercenH}%)
-Bots - **${Bots.size}** (${PercenB}%)
-Online - **${Online.size}** | Idle - **${Idle.size}** | Do Not Distrub - **${Dnd.size}** | Offline - **${Offline.size}**
-Playing - **${Players.size}**
-Discord In Website - **${Websites.size}** | Desktop - **${Desktop.size}** | Mobile - **${Mobile.size}**`)
-    .setFooter(`Requested By ${message.author.username}`)
-    .setTimestamp();
-
+    .setAuthor(`${message.guild.name} Members`, message.guild.iconURL())
+    .addField(`Information`,`
+• **Total:**  ${message.guild.memberCount}
+• **Human:**  ${Humans.size} - \`${PercenH}%\`
+• **Bots:**  ${Bots.size} - \`${PercenB}%\`
+• **Online:** ${Online.size} | **Idle:** ${Idle.size} | **Do Not Distrub:** ${Dnd.size} | **Offline:** ${Offline.size}
+• **Playing:** ${Players.size}
+• **Discord In Website:**  ${Websites.size} | **Desktop:**  ${Desktop.size} | **Mobile:**  ${Mobile.size}`)
     return message.channel.send(Embed);
   }
 };
