90949cafe08058e53048bb87a948142786a53362
Glitch (brooker) <none>
2021-04-07 04:48:33 +0000

    🚿👔 Checkpoint ./commands/Moderation/embed.js:16160679/497

--- commands/Moderation/embed.js
+++ commands/Moderation/embed.js
@@ -4,7 +4,17 @@ module.exports = {
   name: "embed",
   description: "Create embed message",
   category: "Moderation",
-  usage: "embed <title> | <description> | [colour]",
+  usage: "`embed <title> | <description> | [#hex color]`",
+  detail: `**Example**
+\`\`\`
+- Example 1
+{prefix}embed Your Title here | Your Description Here\` | \`#Hex Color\`
+
+- Example 2
+{prefix}embed New Update Channel | 
+- Added new channel for bot command
+- Added new private channel for VIP | #ffff
+\`\`\``,
   botPermission: ["MANAGE_MESSAGES"],
   authorPermission: ["MANAGE_MESSAGES"],
   aliases: [""],
@@ -19,6 +29,7 @@ module.exports = {
     .setTitle(title)
     .setDescription(description)
     .setFooter(message.member.nickname ? message.member.displayName : message.author.username, message.author.displayAvatarURL())
+    .setTimestamp()
     message.channel.send(embed)
     
   }
