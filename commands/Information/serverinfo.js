7112999f0b9572c60fb8390a5a86627e988ce715
Glitch (brooker) <none>
2021-04-15 00:33:34 +0000

    👳🐦 Checkpoint ./commands/Information/serverinfo.js:16160679/579 ./watch.json:16160679/1

--- commands/Information/serverinfo.js
+++ commands/Information/serverinfo.js
@@ -35,16 +35,17 @@ module.exports = {
   
   
   let serverembed = new Discord.MessageEmbed().setColor(client.config.color)
-.setAuthor(guild.name, guild.iconURL())
+.setAuthor(guild.name, guild.iconURL()).setThumbnail(guild.iconURL())
 .addField(`Information`,`
 • **Owners:** ${guild.owner.user.tag}
 • **ID:** ${guild.id}
 • **Region:** ${guild.region}
 • **Verification:** ${lvl[guild.verificationLevel]}
-• **Boost:** ${boost}
-• **Text:** ${text} | **Voice:** ${voice} | **Category:** ${category} 
-• **Channel:** ${channel} | **News:** ${news}`)
-.addField(`Roles [${guild.roles.cache.size || " "}]`, role)
+• **Roles:** ${guild.roles.cache.size}
+• **MemberCount:** ${guild.memberCount}
+• **Boost:** ${boost}`)
+.addField(`Channel [${channel}]`,
+`<:text:832048400018178148> ${text} | <:voice:832048400119496724> ${voice} | <:categoria:832048400814833675> ${category} | <:anuncio:832048399855124490> ${news}`)
 .addField(`Emoji [${guild.emojis.cache.size}]`, emojis)
 .setFooter(`Server created at ${moment.utc(guild.createdAt).format('MM/DD/YY LT')}`)
 message.channel.send(serverembed);
