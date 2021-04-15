e9d256d6f2fa28da88f27cb2b369deceb0af5fc8
Glitch (brooker) <none>
2021-04-15 00:44:59 +0000

    📼📒 Checkpoint ./events/message.js:16160679/272

--- events/message.js
+++ events/message.js
@@ -219,10 +219,5 @@ module.exports.run = async (client, message) => {
 
   //-----------------------------------------------------------------------------------------------------------------
     
-   if (command) command.run(client, message, args).catch(e => {
-    let embed = new MessageEmbed().setColor(client.config.color)
-    .setTitle('There was error command')
-    .setDescription(`\`Error [${command.name}]:\`\n\`\`\`js\n${e}\n\`\`\``)
-    return client.channels.cache.get('801988747205935144').send(embed)
-   })
+   if (command) command.run(client, message, args)
 }
