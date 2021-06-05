a24775c022e70a632e9a9235c358bcb29433e43b
Glitch (brooker) <none>
2021-06-05 05:34:30 +0000

    🍢🐟 Checkpoint ./commands/General/samp.js:16160679/5397

--- commands/General/samp.js
+++ commands/General/samp.js
@@ -1,5 +1,6 @@
-const discord = require('discord.js')
-var query = require('samp-query')
+const Discord = require("discord.js");
+const fetch = require("node-fetch")
+const { MessageButton, MessageActionRow } = require('discord-buttons');
 
 module.exports = {
   name: "samp",
@@ -10,77 +11,47 @@ module.exports = {
   cooldown: 2000,
   run: async (client, message, args) => { 
     
-    let wrong = new discord.MessageEmbed().setColor(client.config.color) 
-    .setAuthor(message.author.username, message.author.displayAvatarURL())
-    .setDescription(`Invalid Argument!`)
-    let xdemb = new discord.MessageEmbed().setColor(client.config.color) 
-    .setAuthor(message.author.username, message.author.displayAvatarURL())
-    .setTitle("Missing Arguments!")
-    .setDescription("Usage: `samp <IP:Address>`")
-    .setTimestamp();  
     
-    if(!args[0]) return message.channel.send(xdemb)
-    if(!message.content.includes('.')) return message.channel.send(wrong)
-    
-     var options = {
-     host: args[0].split(':')[0],
-     port: args[0].split(':')[1]
-     }
+var novc = new Discord.MessageEmbed()
 
-     query(options, async (error, response) => {
-       if(error) {
-       let embed = new discord.MessageEmbed().setColor(client.config.color)
-       .setTitle('There was error')
-       .setDescription(`ERROR: ${error}`)
-       message.channel.send(embed)
-       } else if (response) {
-         
-         if(args[1] === "player" || args[1] === "players") {
-          let embed = new discord.MessageEmbed().setColor(client.config.color) 
-          .setTitle(`${response.hostname}`)
-          .setDescription(`\`\`\`\n${response.players.map(user => `(${user.id})${user.name}`).join(', ') || "To much player to display or none player"}\n\`\`\``)
-          .setFooter(`There are ${response.players.map(user => user.name).length} players online`)
-          
-          return message.channel.send(embed)
-         }
-         
-       let embed = new discord.MessageEmbed().setColor(client.config.color)
-       .setTitle(response.hostname)
-       .addField("Information", 
-`\`\`\`
-Hostname   :: ${response.hostname || "-"}
-Gamemode   :: ${response.gamemode || "-"}
-Language   :: ${response.mapname || "-"}
-Passworded :: ${response.passworded ? 'yes' : 'no' || "-"}
-Maxplayers :: ${response.maxplayers || "-"}
-Online     :: ${response.online || "-"}
-\`\`\``)
-        .addField("Rule", 
-`\`\`\`
-Lagcomp    :: ${response.rules.lagcomp ? 'On' : 'Off'}
-Mapname    :: ${response.rules.mapname || "-"}
-Version    :: ${response.rules.version || "-"}
-Weather    :: ${response.rules.weather || "-"}
-Weburl     :: ${response.rules.weburl || "-"}
-Worldtime  :: ${response.rules.worldtime || "-"}
-\`\`\``)
-       .addField("Players",
-`\`\`\`${response.players.map(user => user.name).length > 10 ? 
-      `${response.players.map(user => user.name).slice(0, 10).join(', ')} and ${response.players.map(user => user.name).length - 10} more....` : 
-       response.players.map(user => user.name).join(', ') || "To many players to display or none"}\`\`\``)
-       
-//        let btn = new disbut.MessageButton()
-// 		   .setStyle('red')
-// 		   .setLabel('Button Red')
-// 		   .setID('click');
-       
-//        let btn2 = new disbut.MessageButton()
-// 		   .setStyle('red')
-// 		   .setLabel('Button Red')
-// 		   .setID('click');
-       
-       await message.channel.send(embed) 
-         
-       }
-     })
+.setTitle("Woops!")
+.setDescription("You're not in a voice channel")
+
+var poker = new Discord.MessageEmbed()
+
+.setTitle("Poker")
+.setDescription("Press the button below to play poker!")
+    
+    let channel = message.member.voice.channel;
+    if(!channel) return message.channel.send(novc)
+    
+    fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+    method: "POST",
+    body: JSON.stringify({
+        max_age: 0,
+        max_uses: 0,
+        target_application_id: "667743057227153408",
+        target_type: 2,
+        temporary: false,
+        validate: null
+    }),
+    headers: {
+        "Authorization": `Bot kntl`,
+        "Content-Type": "application/json"
+    }
+})
+    .then(res => res.json())
+    .then(invite => {
+console.log("sh https://discord.com/invite/awjdoad")
+
+let wtf = new MessageButton()
+.setStyle('url')
+.setLabel('Play') 
+.setEmoji('🎴')
+.setURL(`https://discord.com/invite/${invite.code}`)
+
+let wtf2 = new MessageActionRow()
+.addComponent(wtf)
+
+message.channel.send("", { embed: poker, component: wtf2})
   }}
