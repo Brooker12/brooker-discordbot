73c5939903514669b318f0d14d1482dd11d5ad72
Glitch (brooker) <none>
2021-06-05 06:33:52 +0000

    💒⛪️ Checkpoint ./events/clickButton.js:16160679/197

--- events/clickButton.js
+++ events/clickButton.js
@@ -1,11 +1,8 @@
-const { Client, MessageEmbed } = require('discord.js'); 
-const client = new Client(); 
-require('discord-buttons')(client);
 
 module.exports.run = async (button) => {
   
   if (button.id === 'samp-refresh') {
-     await button.channel.send('Hellow');
+     await button.reply.send('My message');
   }
   
 }
