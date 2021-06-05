0a4efb81f44ffdcd94e8ed4c64a8c7f302e313a8
Glitch (brooker) <none>
2021-06-05 06:10:36 +0000

    🛬🐫 Checkpoint ./events/clickButtons.js:16160679/510

--- events/clickButtons.js
+++ events/clickButtons.js
@@ -2,11 +2,15 @@ const { Client, MessageEmbed } = require('discord.js');
 const client = new Client(); 
 require('discord-buttons')(client);
 
-module.exports.run = async(button) => {
-  
-  // commands/General/samp.js 
-  if(button.id === 'samp-refresh') {
-    await button.reply.send('My message');
+module.exports.run = async (button) => {
+  if (button.id === 'samp-refresh') {
+    
+    let embed = new MessageEmbed()
+    .setDescription('This is help page 2 the red one')
+    .setTitle('Help page 2')
+    .setColor('#FF0000')
+    .setFooter('Click on the buttons for other pages') 
+    button.reply.send(embed);
+    
   }
-  
 }
