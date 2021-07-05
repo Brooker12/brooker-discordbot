005d2f18a03f2cafe4059be5b5015a620cf1d104
Glitch (brooker) <none>
2021-07-05 06:41:59 +0000

    ✍️🎂 Checkpoint ./bot.js:16160679/172 ./events/ready.js:16160679/1032 ./package.json:16160679/5968 ./watch.json:16160679/1

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,21 @@
 const discord = require('discord.js')
 const config = require('../config.json')
+const { Slash } = require('discord-slash-commands');
 
 module.exports.run = async client => {
+  
+  const slash = new Slash(client);
+  
+    slash.command({
+        guildOnly: true,
+        guildID: "788251058630688798",
+        data: {
+            name: "ping",
+            description: "Ping pong?",
+            type: 4,
+            content: `Pong! \`${client.ws.ping}ms\``
+        }
+    })
   
 	console.log(`I am ready! Logged in as ${client.user.tag}!`);
 	console.log(`Service in ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
