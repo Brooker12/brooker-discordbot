b2d84843945fc5bb67a0ed759538dfd98c0331bb
Glitch (brooker) <none>
2021-07-05 09:44:34 +0000

    🎿🐙 Checkpoint ./events/ready.js:16160679/162

--- events/ready.js
+++ events/ready.js
@@ -8,20 +8,15 @@ module.exports.run = async client => {
   
   const slash = new Slash.Client(client, client.user.id)
   
-  const cmd = await slash.postCommand({name: "test", description:"Test Slash Command"}, "788251058630688798")
-  console.log(cmd)
-  
   client.ws.on("INTERACTION_CREATE", async interaction => { 
   
   const inter = await slash.parseInteraction(interaction)
-  if(inter.name === "test") {
-    inter.reply('Test', { ephermal: true })
-  } else if(inter.name === "ping") {
-    inter.reply("Pingtol")
-  }
-    
+    if(inter.name === "test") {
+      inter.reply('Test', { ephermal: true })
+    } else if(inter.name === "ping") {
+      inter.reply("Pingtol")
+    }  
   })
-  
 
 	console.log(`I am ready! Logged in as ${client.user.tag}!`);
 	console.log(`Service in ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
