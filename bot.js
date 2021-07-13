4f9acfe1494193307d1f0c3de39c87834ce89836
Glitch (brooker) <none>
2021-07-13 12:57:25 +0000

    🎗🍏 Checkpoint ./public/style.css:16160679/22498 ./bot.js:16160679/27 ./views/Public/style.css:16160679/22498 ./views/Public/script.js:16160679/107

--- bot.js
+++ bot.js
@@ -25,7 +25,7 @@ var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.use(bodyParser.json());
 app.use(express.static("views"));
-app.use(express.static("public")); 
+app.use(express.static("views/Public")); 
    
 app.set("views", path.join(__dirname, "/views"))
 app.set("view engine", "ejs")
@@ -38,7 +38,7 @@ client.snipes = new Map();
 
 try{
   ["command", "events"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
+    require(`./utils/${handler}`)(client);
   });
 } catch (e){
   console.error(e);
