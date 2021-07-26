06e1963d5a76857e547b5fba2ede5d4d7f8a9591
Glitch (brooker) <none>
2021-07-26 11:10:10 +0000

    🐪😬 Checkpoint ./views/partner/partner-show.ejs:16160679/1196 ./views/Public/style.css:16160679/220 ./views/Public/script.js:16160679/102

--- views/Public/script.js
+++ views/Public/script.js
@@ -1,3 +1,6 @@
 function myFunction() {
     document.getElementsByClassName("topnav")[0].classList.toggle("responsive")
 }  
+function commentDropdown() {
+  document.getElementById("comment-content").classList.toggle("show");
+}
