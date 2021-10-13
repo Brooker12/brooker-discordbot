d4fb6a74e6a99718c3083f9bb03b5595c34211e3
Glitch (brooker) <none>
2021-10-13 03:53:09 +0000

    🍋🍿 Checkpoint ./commands/Configuration/ignore-channel.js:16160679/9515 ./commands/Configuration/config.js:16160679/445

--- commands/Configuration/config.js
+++ commands/Configuration/config.js
@@ -1,169 +1,169 @@
-// let { MessageEmbed } = require('discord.js')
-// //let { MessageButton, MessageActionRow } = require('discord-buttons')
-// let db = require('quick.db')
+let { MessageEmbed } = require('discord.js')
+//let { MessageButton, MessageActionRow } = require('discord-buttons')
+let db = require('quick.db')
 
-// module.exports = {
-//   name: 'config',
-//   description: 'Display all information in configuration',
-//   usage: 'settings',
-//   category: 'Configuration',
-//   authorPermissions: ['MANAGE_GUILD'],
-//   aliases: ['settings'],
-//   run: async(client, message, args) => {
+module.exports = {
+  name: 'config',
+  description: 'Display all information in configuration',
+  usage: 'settings',
+  category: 'Configuration',
+  authorPermissions: ['MANAGE_GUILD'],
+  aliases: ['settings'],
+  run: async(client, message, args) => {
     
-//     /*================ Prefix ===================*/
-//     let prefix = client.config.prefix
+    /*================ Prefix ===================*/
+    let prefix = client.config.prefix
   
-//     /*================ Custom Commands ===================*/
-//     let custom = db.get(`cmd_${message.guild.id}`)
-//     let cmd;
-//     if(custom && custom.length) {
-//       cmd = `\`${custom.map(a => `${a.name}`).join('`, `')}\``
-//     } else {
-//      cmd = '[ None Commands ]'
-//     }
-//     /*================ Ignore Channel ===================*/
-//     let ignores = await db.fetch(`ignorech_${message.guild.id}.channel`)
-//     if(ignores === null || ignores === undefined || ignores.length === 0) ignores = "None"
-//     let igcmd = [];
-//     if (ignores === 'None') {
-//        igcmd = "[ None ]"
-//     } else {
-//        igcmd = ignores.map(e => `${client.channels.cache.get(e) ? client.channels.cache.get(e) : "#DeletedChannel"}`).join(', ') || "None"
-//     }
-//     /*================ Ignore Commands ===================*/
-//    let ignorex = db.get(`ignore_${message.guild.id}.command`)
-//    let igch = [];
-//     if(ignorex === null || ignorex === undefined || ignorex.length === 0) {
-//       igch = '[ None ]'
-//     } else {
-//       igch = "`" + igch.join("`, `") + '`'
-//     }  
-//     /*================ Leveling ===================*/
-//     let leveling = await db.fetch(`level_${message.guild.id}.channel`)
-//     let lvltg = await db.fetch(`level_${message.guild.id}.toggle`)
-//     let lvlch = client.channels.cache.get(leveling)
-//     if (lvlch === null) lvlch = "[ Not set ]"
-//     if (lvlch === undefined) lvlch = "[ Not set ]"
-//     /*================ Rewards Role ===================*/
-//      let rewards = db.get(`rolerewards_${message.guild.id}.reward`)
-//      let roles;
-//     if(rewards === null || rewards === undefined || rewards.length === 0) {
-//       roles = '[ None ]'
-//     } else {
-//       roles = rewards.sort((a, b) => a.level - b.level)
-//               .map(e =>`• Level **${e.level}** - ${message.guild.roles.cache.get(e.roles) ? 
-//                                                                               message.guild.roles.cache.get(e.roles) : "DeletedRoles"}`).join("\n");
-//     }
-//     /*================ Welomcers ===================*/
-//     let weltg = await db.fetch(`welcome_${message.guild.id}.toggle`)
-//     let welmsg = await db.fetch(`welcome_${message.guild.id}.msg`)
-//     if (welmsg === null || welmsg === undefined) welmsg = "[ Default by bot ]"
+    /*================ Custom Commands ===================*/
+    let custom = db.get(`cmd_${message.guild.id}`)
+    let cmd;
+    if(custom && custom.length) {
+      cmd = `\`${custom.map(a => `${a.name}`).join('`, `')}\``
+    } else {
+     cmd = '[ None Commands ]'
+    }
+    /*================ Ignore Channel ===================*/
+    let ignores = await db.fetch(`ignorech_${message.guild.id}.channel`)
+    if(ignores === null || ignores === undefined || ignores.length === 0) ignores = "None"
+    let igcmd = [];
+    if (ignores === 'None') {
+       igcmd = "[ None ]"
+    } else {
+       igcmd = ignores.map(e => `${client.channels.cache.get(e) ? client.channels.cache.get(e) : "#DeletedChannel"}`).join(', ') || "None"
+    }
+    /*================ Ignore Commands ===================*/
+   let ignorex = db.get(`ignore_${message.guild.id}.command`)
+   let igch = [];
+    if(ignorex === null || ignorex === undefined || ignorex.length === 0) {
+      igch = '[ None ]'
+    } else {
+      igch = "`" + igch.join("`, `") + '`'
+    }  
+    /*================ Leveling ===================*/
+    let leveling = await db.fetch(`level_${message.guild.id}.channel`)
+    let lvltg = await db.fetch(`level_${message.guild.id}.toggle`)
+    let lvlch = client.channels.cache.get(leveling)
+    if (lvlch === null) lvlch = "[ Not set ]"
+    if (lvlch === undefined) lvlch = "[ Not set ]"
+    /*================ Rewards Role ===================*/
+     let rewards = db.get(`rolerewards_${message.guild.id}.reward`)
+     let roles;
+    if(rewards === null || rewards === undefined || rewards.length === 0) {
+      roles = '[ None ]'
+    } else {
+      roles = rewards.sort((a, b) => a.level - b.level)
+              .map(e =>`• Level **${e.level}** - ${message.guild.roles.cache.get(e.roles) ? 
+                                                                              message.guild.roles.cache.get(e.roles) : "DeletedRoles"}`).join("\n");
+    }
+    /*================ Welomcers ===================*/
+    let weltg = await db.fetch(`welcome_${message.guild.id}.toggle`)
+    let welmsg = await db.fetch(`welcome_${message.guild.id}.msg`)
+    if (welmsg === null || welmsg === undefined) welmsg = "[ Default by bot ]"
     
-//     let welcome = await db.fetch(`welcome_${message.guild.id}.channel`)
-//     let welch = client.channels.cache.get(welcome)
-//     if (welch === undefined || welch === null) welch = "[ Not set ]"
-//     /*================ Welomcers ===================*/
-//     let levtg = await db.fetch(`leave_${message.guild.id}.toggle`)
-//     let levmsg = await db.fetch(`leave_${message.guild.id}.msg`)
-//     if (levmsg === null || levmsg === undefined) levmsg = "[ Default by bot ]"
+    let welcome = await db.fetch(`welcome_${message.guild.id}.channel`)
+    let welch = client.channels.cache.get(welcome)
+    if (welch === undefined || welch === null) welch = "[ Not set ]"
+    /*================ Welomcers ===================*/
+    let levtg = await db.fetch(`leave_${message.guild.id}.toggle`)
+    let levmsg = await db.fetch(`leave_${message.guild.id}.msg`)
+    if (levmsg === null || levmsg === undefined) levmsg = "[ Default by bot ]"
     
-//     let leave = await db.fetch(`leave_${message.guild.id}.channel`)
-//     let levch = client.channels.cache.get(leave)
-//     if (levch === undefined || levch === null) levch = "[ Not set ]"
+    let leave = await db.fetch(`leave_${message.guild.id}.channel`)
+    let levch = client.channels.cache.get(leave)
+    if (levch === undefined || levch === null) levch = "[ Not set ]"
 
     
-//       const cf1 = new MessageEmbed().setColor(client.config.color)
-//       .setAuthor(`Configurations`, client.user.displayAvatarURL())
-//       .setFooter(`Page 1 of 3`)
-// .setDescription(`
-// **Welcomer System | ${weltg ? 'Enable' : 'Disable'}**
-// • Channel: ${welch}
-// • Message: 
-// \`\`\`
-// ${welmsg}
-// \`\`\`
-// **Leave System | ${levtg ? 'Enable' : 'Disable'}**
-// • Channel: ${levch}
-// • Message: 
-// \`\`\`
-// ${levmsg}
-// \`\`\`
-// `)
+      const cf1 = new MessageEmbed().setColor(client.config.color)
+      .setAuthor(`Configurations`, client.user.displayAvatarURL())
+      .setFooter(`Page 1 of 3`)
+.setDescription(`
+**Welcomer System | ${weltg ? 'Enable' : 'Disable'}**
+• Channel: ${welch}
+• Message: 
+\`\`\`
+${welmsg}
+\`\`\`
+**Leave System | ${levtg ? 'Enable' : 'Disable'}**
+• Channel: ${levch}
+• Message: 
+\`\`\`
+${levmsg}
+\`\`\`
+`)
       
-//       const cf2 = new MessageEmbed().setColor(client.config.color)
-//       .setAuthor(`Configurations`, client.user.displayAvatarURL())
-//       .setFooter(`Page 2 of 3`)
-//       .setDescription(`
-// **Leveling | ${lvltg ? 'Enable' : 'Disable'}**
-// • Level Log: ${lvlch}
+      const cf2 = new MessageEmbed().setColor(client.config.color)
+      .setAuthor(`Configurations`, client.user.displayAvatarURL())
+      .setFooter(`Page 2 of 3`)
+      .setDescription(`
+**Leveling | ${lvltg ? 'Enable' : 'Disable'}**
+• Level Log: ${lvlch}
 
-// **Rewards Roles | ${rewards ? rewards.length : 0}**
-// ${roles}
+**Rewards Roles | ${rewards ? rewards.length : 0}**
+${roles}
 
-// **Guild Prefix**
-// • Prefix: ${prefix}
-// `)
+**Guild Prefix**
+• Prefix: ${prefix}
+`)
 
-//       const cf3 = new MessageEmbed().setColor(client.config.color)
-//       .setAuthor(`Configurations`, client.user.displayAvatarURL())
-//       .setFooter(`Page 3 of 3`)
-//       .setDescription(`
-// **Custom Commands | ${custom ? custom.length : 0}**
-// ${cmd}
+      const cf3 = new MessageEmbed().setColor(client.config.color)
+      .setAuthor(`Configurations`, client.user.displayAvatarURL())
+      .setFooter(`Page 3 of 3`)
+      .setDescription(`
+**Custom Commands | ${custom ? custom.length : 0}**
+${cmd}
 
-// **Ignore Commands | ${ignorex ? ignorex.length : 0}**
-// ${igch}
+**Ignore Commands | ${ignorex ? ignorex.length : 0}**
+${igch}
 
-// **Ignore Channels | ${ignores ? ignores.length : 0}**
-// ${igcmd}
-// `)
+**Ignore Channels | ${ignores ? ignores.length : 0}**
+${igcmd}
+`)
       
-//   let next = new MessageButton().setStyle('grey').setID('next').setLabel('>>')
-//   let back = new MessageButton().setStyle('grey').setID('back').setLabel('<<')
+  let next = new MessageButton().setStyle('grey').setID('next').setLabel('>>')
+  let back = new MessageButton().setStyle('grey').setID('back').setLabel('<<')
   
-//   let nextDisable = new MessageButton().setStyle('grey').setID('next-disable').setLabel('>>').setDisabled();
-//   let backDisable = new MessageButton().setStyle('grey').setID('back-disable').setLabel('<<').setDisabled();      
+  let nextDisable = new MessageButton().setStyle('grey').setID('next-disable').setLabel('>>').setDisabled();
+  let backDisable = new MessageButton().setStyle('grey').setID('back-disable').setLabel('<<').setDisabled();      
   
-//   let active = new MessageActionRow().addComponent([back, next])
-//   let disable = new MessageActionRow().addComponent([backDisable, nextDisable])
+  let active = new MessageActionRow().addComponent([back, next])
+  let disable = new MessageActionRow().addComponent([backDisable, nextDisable])
   
-//   let embed = [cf1, cf2, cf3]
+  let embed = [cf1, cf2, cf3]
   
-//   message.channel.send({embed: embed[0], components:active}).then(msg => {
+  message.channel.send({embed: embed[0], components:active}).then(msg => {
     
-//    const collector = msg.createButtonCollector((button) => message.author.id === message.author.id, {time: 300000});
+   const collector = msg.createButtonCollector((button) => message.author.id === message.author.id, {time: 300000});
     
-//     let pages = 0;
+    let pages = 0;
     
-//     collector.on('collect', btn => {
-//       btn.reply.defer()
-//       if(btn.clicker.user.id === message.author.id) {
-//         if(btn.id === 'back') {
-//           if(pages !== 0) {
-//             --pages
-//             msg.edit({embed: embed[pages], components: active})
-//           } else {
-//             pages = embed.length - 1
-//             msg.edit({embed: embed[pages], components: active})
-//           }
-//         } else if(btn.id === 'next') {
-//           if(pages < embed.length - 1) {
-//             pages++
-//             msg.edit({embed: embed[pages], components: active})
-//           } else {
-//             pages = 0;
-//             msg.edit({embed: embed[pages], components: active})
-//           }
-//         }
-//       }
-//     })
-//     collector.on('end', btn => {
-//       if(msg) {
-//         msg.edit('This message has disabled', {embed: cf1, components: disable})
-//       }
-//     })
+    collector.on('collect', btn => {
+      btn.reply.defer()
+      if(btn.clicker.user.id === message.author.id) {
+        if(btn.id === 'back') {
+          if(pages !== 0) {
+            --pages
+            msg.edit({embed: embed[pages], components: active})
+          } else {
+            pages = embed.length - 1
+            msg.edit({embed: embed[pages], components: active})
+          }
+        } else if(btn.id === 'next') {
+          if(pages < embed.length - 1) {
+            pages++
+            msg.edit({embed: embed[pages], components: active})
+          } else {
+            pages = 0;
+            msg.edit({embed: embed[pages], components: active})
+          }
+        }
+      }
+    })
+    collector.on('end', btn => {
+      if(msg) {
+        msg.edit('This message has disabled', {embed: cf1, components: disable})
+      }
+    })
       
-//    })
-//   }
-// }
+   })
+  }
+}
--- commands/Configuration/ignore-channel.js
+++ commands/Configuration/ignore-channel.js
@@ -1,131 +1,77 @@
-// const db = require("quick.db")
-// const { MessageMenu, MessageMenuOption } = require('discord-buttons')
-// const { MessageEmbed } = require('discord.js')
+const db = require("quick.db")
+const { MessageMenu, MessageMenuOption } = require('discord-buttons')
+const { MessageEmbed } = require('discord.js')
 
-// module.exports = {
-//   name: "ignore-channel",
-//   usage: `\`ignorech [ on || off || <#channel> ]\``,
-//   detail: `**Information**
-// \`\`\`
-// - Set Channel
-// {prefix}ignorech <#channel>
+module.exports = {
+  name: "ignore-channel",
+  usage: `\`ignorech [ on || off || <#channel> ]\``,
+  detail: `**Information**
+\`\`\`
+- Set Channel
+{prefix}ignorech <#channel>
 
-// - Delete Channel
-// {prefix}ignorech <#channel>
-// Note: Mention a channel that is in the database
-// \`\`\``,
-//   description:`Ignore users to use bot command in the channel.`,
-//   authorPermission: ["MANAGE_GUILD"],
-//   category: "Configuration",
-//   aliases: ['igch', 'ignorech'],
-//   run: async(client, message, args) => { 
+- Delete Channel
+{prefix}ignorech <#channel>
+Note: Mention a channel that is in the database
+\`\`\``,
+  description:`Ignore users to use bot command in the channel.`,
+  authorPermission: ["MANAGE_GUILD"],
+  category: "Configuration",
+  aliases: ['igch', 'ignorech'],
+  run: async(client, message, args) => { 
     
-//   let channel = message.mentions.channels.first();
+  let channel = message.mentions.channels.first();
     
-//   let ignores = await db.fetch(`ignorech_${message.guild.id}.channel`)
-//   if(ignores === null || ignores === undefined || ignores.length === 0) ignores = "None"
+  let ignores = await db.fetch(`ignorech_${message.guild.id}.channel`)
+  if(ignores === null || ignores === undefined || ignores.length === 0) ignores = "None"
     
-//     if (!args[0]) {  
+    if (!args[0]) {  
     
-//     let ch4 = [];
-//     if (ignores === 'None') {
-//        ch4 = "[ None ]"
-//     } else {
-//        ch4 = ignores.map(e => `${client.channels.cache.get(e) ? client.channels.cache.get(e) : "#DeletedChannel"}`).join(', ') || "None"
-//     }
+    let ch4 = [];
+    if (ignores === 'None') {
+       ch4 = "[ None ]"
+    } else {
+       ch4 = ignores.map(e => `${client.channels.cache.get(e) ? client.channels.cache.get(e) : "#DeletedChannel"}`).join(', ') || "None"
+    }
 
-//       const emb = new MessageEmbed()
-//       .setAuthor('Channel that are ignore', client.user.displayAvatarURL()).setColor(client.config.color)
-//       .setDescription(`${ch4 || "[ Not set. ]"}`)
-//       .setFooter(`Read more ${client.config.prefix}help ${module.exports.name}`)
+      const emb = new MessageEmbed()
+      .setAuthor('Channel that are ignore', client.user.displayAvatarURL()).setColor(client.config.color)
+      .setDescription(`${ch4 || "[ Not set. ]"}`)
+      .setFooter(`Read more ${client.config.prefix}help ${module.exports.name}`)
       
-            
-//       let menus = new MessageMenu()
-//       .setID('ignore-channel') 
-//       .setMaxValues(3) 
-//       .setMinValues(1) 
-//       .setPlaceholder('List Channels');  
-
-//       let opsii =  message.guild.channels.cache.filter((c) => c.type === "text" && c.permissionsFor(message.guild.me).has('MANAGE_CHANNELS'))
+      message.channel.send(emb)
       
-//       let kelebihan = false;
-//       if(opsii.size > 25) kelebihan = true;
-//       else {
-//        opsii.forEach(opsi => {
-//         let option = new MessageMenuOption()
-//          .setLabel(opsi.name.length > 25 ? opsi.name.slice(0, 25) : opsi.name)
-//         .setValue(opsi.id) 
-//         if(ignores && ignores.includes(opsi.id)) option.setDescription(`Channel has added, it'll be removed if you choose`)
-//         .setDefault()
-//         menus.addOption(option)
-//        })
-//       }
-      
-//       let msg;
-//       if(kelebihan) message.channel.send({content: '`Menus cannot displayed: Channel over 25`', embed:emb})
-//       else msg = await message.channel.send(emb, menus)
-      
-//       client.on('clickMenu', async (menu) => {
-//         if(menu.message.id === msg.id) {
-//             if(menu.clicker.user.id === message.author.id) {
-//               let embed = new MessageEmbed().setColor(client.config.color)
-//               .setAuthor(`Ignore Channels`, client.user.displayAvatarURL())
-//               let chDeleted = [];
-//               let chAdded = [];
-//               menu.values.forEach(ch => {
-//                 if(ignores.includes(ch)) {
-//                   let data = ignores.find(x => x === ch)
-//                   let value = ignores.indexOf(ch)
-//                   delete ignores[value]
-                  
-//                   var filter = ignores.filter(x => {
-//                     return x != null && x != ''
-//                   })
-//                   db.set(`ignorech_${message.guild.id}.channel`, filter)
-//                   chDeleted.push(`<#${ch}>`)
-//                 } else {
-//                   db.push(`ignorech_${message.guild.id}.channel`, ch)
-//                   chAdded.push(`<#${ch}>`)
-//                 }
-//               })
-//               if (chAdded.length > 0) embed.addField(`Added`, chAdded.map(x => x).join(', '))
-//               if (chDeleted.length > 0) embed.addField(`Removed`, chDeleted.map(x => x).join(', '))
-//               menu.message.update({embed: embed, component: null})
-//             } else menu.reply.send("You're not allowed to use this menus.", true)
-//           }
-//       })
-      
-//   } else {
-//      if(!channel) {
-//         let wrong = new MessageEmbed().setColor(client.config.color) 
-//         .setAuthor('Ignore Channel', client.user.displayAvatarURL())
-//         .setDescription(`Invalid Argument!`)
+  } else {
+     if(!channel) {
+        let wrong = new MessageEmbed().setColor(client.config.color) 
+        .setAuthor('Ignore Channel', client.user.displayAvatarURL())
+        .setDescription(`Invalid Argument!`)
   
-//         message.channel.send(wrong)
-//       } else if(ignores.includes(channel.id)) {
-//          let database = db.get(`ignorech_${message.guild.id}.channel`)
-//          let data = database.find(x => x === channel.id)
-//          let value = database.indexOf(data)
-//          delete database[value]
+        message.channel.send(wrong)
+      } else if(ignores.includes(channel.id)) {
+         let database = db.get(`ignorech_${message.guild.id}.channel`)
+         let data = database.find(x => x === channel.id)
+         let value = database.indexOf(data)
+         delete database[value]
 
-//          var filter = database.filter(x => {
-//          return x != null && x != ''
-//          })
-//          db.set(`ignorech_${message.guild.id}.channel`, filter) 
+         var filter = database.filter(x => {
+         return x != null && x != ''
+         })
+         db.set(`ignorech_${message.guild.id}.channel`, filter) 
         
-//         let already = new MessageEmbed().setColor(client.config.color)
-//         .setAuthor('Ignore Channel', client.user.displayAvatarURL())
-//         .setDescription(`ignorech channel has been delete **${channel.name}**`)   
+        let already = new MessageEmbed().setColor(client.config.color)
+        .setAuthor('Ignore Channel', client.user.displayAvatarURL())
+        .setDescription(`ignorech channel has been delete **${channel.name}**`)   
     
-//         message.channel.send(already)
-//       } else {
-//           db.push(`ignorech_${message.guild.id}.channel`, channel.id) 
+        message.channel.send(already)
+      } else {
+          db.push(`ignorech_${message.guild.id}.channel`, channel.id) 
           
-//           let embed = new MessageEmbed().setColor(client.config.color) 
-//           .setAuthor('Ignore Channel', client.user.displayAvatarURL()) 
-//           .setDescription(`**${channel.name}** channel has been add to Ignorech`)
+          let embed = new MessageEmbed().setColor(client.config.color) 
+          .setAuthor('Ignore Channel', client.user.displayAvatarURL()) 
+          .setDescription(`**${channel.name}** channel has been add to Ignorech`)
           
-//           message.channel.send(embed)
-//       }
-//   }
-// }}
+          message.channel.send(embed)
+      }
+  }
+}}
